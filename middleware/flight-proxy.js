f040d775170fcc1a7fc0bf4436199be9ca784245
Vivek Kumar Singh <64981263+singhvivekkumar@users.noreply.github.com>
2024-06-19 16:59:12 +0530

    Refactor: remove redis cache

--- middleware/flight-proxy.js
+++ middleware/flight-proxy.js
@@ -1,18 +1,18 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 const { SEARCH_SERVICE } = require("../config/serverConfig");
-const client = require("../redis/client");
+// const client = require("../redis/client");
 
-const cacheData = {
-	key: "",
-	value: "",
-};
+// const cacheData = {
+// 	key: "",
+// 	value: "",
+// };
 
-const updateCache = async () => {
+// const updateCache = async () => {
 	//update redis
-	await client.set(`flightQuery:${cacheData.key}`, cacheData.value);
-	await client.expire(`flightQuery:${cacheData.key}`, 300);
-	console.log(cacheData.key, cacheData.value, "hiii");
-};
+	// await client.set(`flightQuery:${cacheData.key}`, cacheData.value);
+	// await client.expire(`flightQuery:${cacheData.key}`, 300);
+	// console.log(cacheData.key, cacheData.value, "hiii");
+// };
 
 const flightProxy = createProxyMiddleware({
 	target: SEARCH_SERVICE,
@@ -21,7 +21,7 @@ const flightProxy = createProxyMiddleware({
 	onProxyReq: (proxyReq, req, res) => {
 		// Modify the request body if needed
 		if (req.body) {
-			cacheData.key = JSON.stringify(req.query);
+			// cacheData.key = JSON.stringify(req.query);
 			proxyReq.setHeader("Content-Type", "application/json");
 			proxyReq.setHeader(
 				"Content-Length",
@@ -31,12 +31,12 @@ const flightProxy = createProxyMiddleware({
 			// console.log(cacheData.key, req.query);
 		}
 	},
-	onProxyRes: (proxyRes, req, res) => {
-		proxyRes.addListener("data", async (chunk) => {
-			cacheData.value = JSON.stringify(JSON.parse(chunk));
-			await updateCache();
-		});
-	},
+	// onProxyRes: (proxyRes, req, res) => {
+	// 	proxyRes.addListener("data", async (chunk) => {
+	// 		cacheData.value = JSON.stringify(JSON.parse(chunk));
+	// 		await updateCache();
+	// 	});
+	// },
 });
 
 
